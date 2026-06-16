e51c32b205ed13c24250ed3978546cd073470762
Alexander Emelyanov <sanchemba@gmail.com>
2026-06-16 12:54:54 +0300

    Added namespaces in urls.py for core and leads

--- zhivaya_poyma/core/urls.py
+++ zhivaya_poyma/core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = "core"
+
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
--- zhivaya_poyma/leads/urls.py
+++ zhivaya_poyma/leads/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = "leads"
+
 urlpatterns = [
     path("", views.join, name="join"),
 ]
