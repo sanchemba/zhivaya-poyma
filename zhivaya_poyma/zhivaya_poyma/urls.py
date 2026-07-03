a8335270c69974c3c0a1896bebe33cbc92a30d49
Alexander Emelyanov <sanchemba@gmail.com>
2026-07-03 16:01:05 +0300

    Media url fixed

--- zhivaya_poyma/zhivaya_poyma/urls.py
+++ zhivaya_poyma/zhivaya_poyma/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, re_path  # Импортируем re_path
+from django.views.static import serve  # Импортируем serve
 from django.conf import settings
 from django.conf.urls.static import static
 from news.ckeditor_views import ckeditor_image_upload
