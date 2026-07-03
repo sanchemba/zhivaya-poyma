220ff353f29cf8cb27f9690ffc7a20fe38604191
Alexander Emelyanov <sanchemba@gmail.com>
2026-07-03 16:49:05 +0300

    Media url fixed

--- zhivaya_poyma/zhivaya_poyma/settings.py
+++ zhivaya_poyma/zhivaya_poyma/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- строго сюда
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,6 +159,10 @@ STATIC_ROOT = BASE_DIR.parent.parent / "public_html" / "static"
 MEDIA_URL = "/media/"
 #MEDIA_ROOT = '/home/users/j/j93531404/domains/xn--80aaepkivoi3n.xn--p1ai/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
+# Разрешаем WhiteNoise обрабатывать не только статику, но и медиа-файлы
+WHITENOISE_AUTOREFRESH = True
+WHITENOISE_USE_FINDERS = True
 
 # Папка, куда будут сохраняться загруженные файлы
 CKEDITOR_UPLOAD_PATH = "uploads/"
