9e530fb3c57569f372844a6098367f04018666e0
Alexander Emelyanov <sanchemba@gmail.com>
2026-07-03 16:42:48 +0300

    Site unworked fixed

--- zhivaya_poyma/zhivaya_poyma/urls.py
+++ zhivaya_poyma/zhivaya_poyma/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from news.ckeditor_views import ckeditor_image_upload
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
