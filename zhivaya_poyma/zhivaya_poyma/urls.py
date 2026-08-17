0372630aded2b956e20ff2e5ead982c342033749
Alexander Emelyanov <sanchemba@gmail.com>
2026-08-17 19:57:43 +0300

    changed journal to news

--- zhivaya_poyma/zhivaya_poyma/urls.py
+++ zhivaya_poyma/zhivaya_poyma/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for zhivaya_poyma project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -27,15 +11,12 @@ from wagtail import urls as wagtail_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
-    path("news/", include("news.urls")),
     path("join/", include("leads.urls")),
 
     path("cms/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
-    path("journal/", include(wagtail_urls)),
 
-    path("ckeditor/upload/", ckeditor_image_upload, name="ckeditor_image_upload"),
-    path("ckeditor/", include("ckeditor_uploader.urls")),
+    path("news/", include(wagtail_urls)),
 ]
 
 if settings.DEBUG:
